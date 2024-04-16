50d35036a6df5360201e6f24effa80e60cdfbafe
hgb926 <hgb926@naver.com>
2024-04-16 11:59:49 +0900

    # 0416 회원관리 시스템 - 회원 정보 생성, 전체 조회 처리

--- src/day06/member/MemberRepository.java
+++ src/day06/member/MemberRepository.java
@@ -16,8 +16,12 @@ public class MemberRepository {
 
     // 메서드
 
-    // 회원정보 생성
+    /**
+     * 생성된 회원정보를 회원 배열에 끝에 추가하는 기능
+     * @param newMember - 사용자의 입력으로 전달된 회원 정보 객체
+     */
     void addNewMember(Member newMember) {
+
         // 배열에 데이터 추가 로직
         Member[] temp = new Member[members.length + 1];
         for (int i = 0; i < members.length; i++) {
@@ -26,5 +30,24 @@ public class MemberRepository {
         temp[temp.length - 1] = newMember;
         members = temp;
     }
+
+
+    /**
+     * 이메일 중복을 확인하는 기능
+     * @param targetEmail - 검사할 사용자의 입력 메일값
+     * @return - 이메일이 존재한다면 true,
+     *           존재하지 않는 사용가능한 이메일이면 false
+     * @author - 닭가슴
+     * @since 2024.04.16
+     */
+    boolean isDuplicateEmail(String targetEmail) {
+        for (Member m : members) {
+            if (targetEmail.equals(m.email)) {
+                return true;
+            }
+        }
+        return false;
+    }
 }
+
 
--- src/day06/member/MemberView.java
+++ src/day06/member/MemberView.java
@@ -1,5 +1,7 @@
 package day06.member;
 
+import util.SimpleInput;
+
 // 역할: 회원 데이터 관리를 위해 입력 출력 담당함
 public class MemberView {
 
@@ -7,8 +9,11 @@ public class MemberView {
     MemberRepository mr;
     // members 배열을 받아와야 출력을 할 수 있다
     // members 배열은 memberRepository 가 가지고 있으므로 받아온다.
+    SimpleInput si;
+
     MemberView() {
         this.mr = new MemberRepository();
+        this.si = new SimpleInput();
     }
 
     void showMembers() {
@@ -17,4 +22,55 @@ public class MemberView {
             System.out.println(m);
         }
     }
+
+    // 회원정보 생성을 위해 입력을 처리
+    void inputNewMember() {
+        String email = null;
+        while (true) {
+            email = si.input("- 이메일: ");
+            if (!mr.isDuplicateEmail(email)) {
+                break;
+            }
+            System.out.println("중복된 이메일입니다.");
+        }
+
+        String name = si.input("- 이름: ");
+        String password = si.input("- 패스워드: ");
+        String gender = si.input("- 성별: ");
+        int age = Integer.parseInt(si.input("- 나이: "));
+
+        // 입력데이터를 기반으로 한 명의 회원 객체를 생성
+        Member newMember = new Member(email, password, name, gender, age);
+
+        // 위임 - 관심사의 분리
+        mr.addNewMember(newMember);
+    }
+
+    // 사용자에게 보여줄 전체 메뉴 화면 출력
+    String showProgramMenu() {
+        System.out.println("\n##### 회원 관리 시스템 #####");
+        System.out.println("* 1. 회원 정보 등록하기");
+        System.out.println("* 2. 개별회원 정보 조회하기");
+        System.out.println("* 3. 전체회원 정보 조회하기");
+        System.out.println("* 4. 회원 정보 수정하기");
+        System.out.println("* 5. 프로그램 종료");
+        System.out.println("==============================");
+
+        String menuNumber = si.input("- 메뉴 번호: ");
+        return menuNumber; // 메뉴컨트롤러에게 번호 전달을 위해 리턴
+    }
+
+    // 프로그램 종료를 판단하는 입출력
+    boolean exitProgram() {
+        String exit = si.input("- 프로그램을 종료합니까? [y/n]\n>>");
+        if (exit.equals("y")) {
+            System.out.println("프로그램을 종료합니다!");
+            return true;
+        } else {
+            System.out.println("프로그램 종료를 취소합니다");
+            return false;
+        }
+
+    }
+
 }
