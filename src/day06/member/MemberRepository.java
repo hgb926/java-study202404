1eb2e2c5b05889b10bfb715c958bb75a8f2ec9a5
hgb926 <hgb926@naver.com>
2024-04-16 17:49:29 +0900

    # 0416 회원관리 시스템 - 탈퇴 회원 처리 전

--- src/day06/member/MemberRepository.java
+++ src/day06/member/MemberRepository.java
@@ -1,20 +1,32 @@
 package day06.member;
 
-// 역할: 회원 배열을 관리하는 역할 - 회원 데이터 저장소 (오로지 이것만)
+// 역할: 회원 배열을 관리하는 역할 - 회원 데이터 저장소
 public class MemberRepository {
 
     // 필드
-    static Member[] members; // 회원을 관리할 배열 생성
+//    static Member[] members; // 현재 관리되는 회원 배열
+//    static Member[] restoreList; // 삭제된 회원들이 모일 배열
+
+    MemberList members;
+    MemberList restoreList;
 
     // 생성자
     MemberRepository() {
-        this.members = new Member[3];
-        members[0] = new Member("abc@def.com","1234","콩순이","여성",50);
-        members[1] = new Member("qhe@hyk.com","2352","팥돌이","남성",30);
-        members[2] = new Member("qyy@zbf.com","4574","팥죽이","여성",26);
+//        this.members = new Member[3];
+//        members[0] = new Member("abc@def.com", "1234", "콩순이", "여성", 50);
+//        members[1] = new Member("ghi@def.com", "5432", "팥돌이", "남성", 40);
+//        members[2] = new Member("xyz@def.com", "7890", "팥죽이", "여성", 30);
+
+        this.members = new MemberList();
+        members.push(new Member("abc@def.com", "1234", "콩순이", "여성", 50));
+        members.push(new Member("ghi@def.com", "5432", "팥돌이", "남성", 40));
+        members.push(new Member("xyz@def.com", "7890", "팥죽이", "여성", 30));
+
+        this.restoreList = new MemberList();
     }
 
     // 메서드
+
 
     /**
      * 생성된 회원정보를 회원 배열에 끝에 추가하는 기능
@@ -22,32 +34,73 @@ public class MemberRepository {
      */
     void addNewMember(Member newMember) {
 
-        // 배열에 데이터 추가 로직
-        Member[] temp = new Member[members.length + 1];
-        for (int i = 0; i < members.length; i++) {
-            temp[i] = members[i];
-        }
-        temp[temp.length - 1] = newMember;
-        members = temp;
-    }
+        // 배열에 데이터를 추가하는 로직
+//        Member[] temp = new Member[members.length + 1];
+//        for (int i = 0; i < members.length; i++) {
+//            temp[i] = members[i];
+//        }
+//        temp[temp.length - 1] = newMember;
+//        members = temp;
 
+        members.push(newMember);
+    }
 
     /**
      * 이메일 중복을 확인하는 기능
-     * @param targetEmail - 검사할 사용자의 입력 메일값
-     * @return - 이메일이 존재한다면 true,
+     * @param targetEmail - 검사할 사용자의 입력 이메일 값
+     * @return - 이메일이 이미 존재한다면 true,
      *           존재하지 않는 사용가능한 이메일이면 false
-     * @author - 닭가슴
+     * @author - 코딩킹
      * @since 2024.04.16
      */
     boolean isDuplicateEmail(String targetEmail) {
-        for (Member m : members) {
-            if (targetEmail.equals(m.email)) {
-                return true;
-            }
-        }
-        return false;
+        return members.findIndex(targetEmail) != -1;
+    }
+
+    /**
+     * 이메일을 통해 회원의 모든 정보(객체)를 가져오는 메서드
+     * @param inputEmail - 사용자가 입력한 이메일값
+     * @return - 해당 이메일을 통해 찾아낸 회원 객체,
+     *           만약 찾지 못하면 null을 리턴
+     */
+    public Member findMemberByEmail(String inputEmail) {
+//        for (Member m : members) {
+//            if (inputEmail.equals(m.email)) {
+//                return m;
+//            }
+//        }
+//        return null;
+
+        return members.get(inputEmail);
+    }
+
+//    int findIndex(String email) {
+//        for (int i = 0; i < members.length; i++) {
+//            if (email.equals(members[i].email)) {
+//                return i;
+//            }
+//        }
+//        return -1;
+//    }
+
+    // 배열에서 회원정보 삭제
+    public void removeMember(String inputEmail) {
+
+//        int index = findIndex(inputEmail);
+        int index = members.findIndex(inputEmail);
+
+        if (index == -1) return;
+
+//        for (int i = index; i < members.length - 1; i++) {
+//            members[i] = members[i + 1];
+//        }
+//
+//        Member[] temp = new Member[members.length - 1];
+//        for (int i = 0; i < temp.length; i++) {
+//            temp[i] = members[i];
+//        }
+//        members = temp;
+
+        members.remove(index);
     }
 }
-
-
--- src/day06/member/MemberView.java
+++ src/day06/member/MemberView.java
@@ -2,13 +2,11 @@ package day06.member;
 
 import util.SimpleInput;
 
-// 역할: 회원 데이터 관리를 위해 입력 출력 담당함
+// 역할: 회원 데이터 관리를 위해 입력 출력을 담당함
 public class MemberView {
 
     // 객체의 협력
     MemberRepository mr;
-    // members 배열을 받아와야 출력을 할 수 있다
-    // members 배열은 memberRepository 가 가지고 있으므로 받아온다.
     SimpleInput si;
 
     MemberView() {
@@ -17,8 +15,8 @@ public class MemberView {
     }
 
     void showMembers() {
-        System.out.printf("========== 현재 회원 목록 (총 %d명) =========\n", mr.members.length);
-        for (Member m : mr.members) {
+        System.out.printf("========= 현재 회원 목록 (총 %d명) ==========\n", mr.members.size());
+        for (Member m : mr.members.getMembers()) {
             System.out.println(m);
         }
     }
@@ -53,24 +51,91 @@ public class MemberView {
         System.out.println("* 2. 개별회원 정보 조회하기");
         System.out.println("* 3. 전체회원 정보 조회하기");
         System.out.println("* 4. 회원 정보 수정하기");
-        System.out.println("* 5. 프로그램 종료");
-        System.out.println("==============================");
+        System.out.println("* 5. 회원 탈퇴하기");
+        System.out.println("* 6. 프로그램 종료");
+        System.out.println("=============================");
 
         String menuNumber = si.input("- 메뉴 번호: ");
-        return menuNumber; // 메뉴컨트롤러에게 번호 전달을 위해 리턴
+        return menuNumber;
     }
 
     // 프로그램 종료를 판단하는 입출력
     boolean exitProgram() {
-        String exit = si.input("- 프로그램을 종료합니까? [y/n]\n>>");
+        String exit = si.input("- 프로그램을 종료합니까? [y/n]\n>> ");
         if (exit.equals("y")) {
             System.out.println("프로그램을 종료합니다!");
             return true;
-        } else {
-            System.out.println("프로그램 종료를 취소합니다");
+        }
+        else {
+            System.out.println("프로그램 종료를 취소합니다.");
             return false;
+        }
+    }
+
+    // 이메일 입력받고 찾은 회원정보를 출력
+    public void getMember() {
+        String inputEmail = si.input("# 조회하실 회원의 이메일을 입력하세요.\n>> ");
+
+        // 이메일이 일치하는 회원이 있는지 조회
+        Member foundMember = mr.findMemberByEmail(inputEmail);
+
+        if (foundMember != null) {
+            System.out.println("============= 조회 결과 ============");
+            System.out.println("# 이름: " + foundMember.memberName);
+            System.out.println("# 비밀번호: " + foundMember.password);
+            System.out.println("# 성별: " + foundMember.gender);
+            System.out.println("# 나이: " + foundMember.age);
+            System.out.println();
+        } else {
+            System.out.println("\n# 해당 회원은 존재하지 않습니다.");
+        }
+    }
+
+    // 수정 대상의 이메일을 입력받고 조회에 성공하면 패스워드를 수정
+    public void updatePassword() {
+        String inputEmail = si.input("# 수정하실 회원의 이메일을 입력하세요.\n>> ");
+
+        // 이메일이 일치하는 회원이 있는지 조회
+        Member foundMember = mr.findMemberByEmail(inputEmail);
+
+        if (foundMember != null) {
+
+            // 기존 비밀번호를 입력해주세요
+
+            // 비번 수정
+            System.out.printf("# %s님의 비밀번호를 변경합니다.\n", foundMember.memberName);
+            String newPassword = si.input("# 새 비밀번호: ");
+
+            // 회원정보 실제로 수정
+//            foundMember.password = newPassword;
+            foundMember.changePassword(newPassword);
+
+            System.out.println("# 비밀번호 변경이 완료되었습니다.");
+        } else {
+            System.out.println("\n# 해당 회원은 존재하지 않습니다.");
+        }
+    }
+
+    public void deleteMember() {
+        String inputEmail = si.input("# 삭제하실 회원의 이메일을 입력하세요.\n>> ");
+
+        // 이메일이 일치하는 회원이 있는지 조회
+        Member foundMember = mr.findMemberByEmail(inputEmail);
+
+        if (foundMember != null) {
+            // 삭제 진행
+            // 패스워드 검사
+            String inputPw = si.input("# 비밀번호를 입력: ");
+            if (inputPw.equals(foundMember.password)) {
+
+                mr.removeMember(inputEmail);
+                System.out.printf("# %s님의 회원정보가 삭제되었습니다.\n", foundMember.memberName);
+            } else {
+                System.out.println("\n# 비밀번호가 일치하지 않습니다. 탈퇴를 취소합니다.");
+            }
+        } else {
+            System.out.println("\n# 해당 회원은 존재하지 않습니다.");
         }
 
     }
-
 }
